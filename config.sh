9b04bb7e987ca374cbd0a468da85076852a65b0a
Divyashet66 <divyagurunathshet@gmail.com>
2022-11-22 12:07:32 +0530

    updated changes

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-deployment_metadata_name=draw-app
+deployment_metadata_name=todo-app
 replicas=1
-selector_name=draw-app
-template_label_name=draw-app
-container_name=draw-app
-image=gcr.io/tech-rnd-project/draw
-container_port=1234
+selector_name=todo-app
+template_label_name=todo-app
+container_name=todo-app
+image=gcr.io/tech-rnd-project/todo
+container_port=3000
 
 kubeContext=gke_tech-rnd-project_us-central1-a_skaffold-cluster
 manifest1=k8/deployment.yaml
@@ -13,9 +13,9 @@ manifest2=k8/service.yaml
 profile=staging
 skaffold_metadata_name=staging-prod-deploy
 
-service_metadata_name=draw-app
-service_port=1234
-service_target_port=1234
+service_metadata_name=todo-app
+service_port=3000
+service_target_port=3000
 type=LoadBalancer
-selector_app=draw-app
+selector_app=todo-app
 
--- mainscript/script.sh
+++ mainscript/script.sh
@@ -3,17 +3,17 @@ pid=$!
 echo "process 1 running"
 wait $pid
 echo "process 1 done"
-containerId='docker run -d -p 1234:1234 --rm --name draw draw'
+containerId='docker run -d -p 3000:3000 --rm --name todo todo'
 echo "process 2 running"
 echo `${containerId}`
 wait $pid
 echo "process 2 done"
-open "http://localhost:1234"
+open "http://localhost:3000"
 pid=$!
 echo "process 3 running"
 wait $pid
 echo "process 3 done"
-docker push gcr.io/tech-rnd-project/draw
+docker push gcr.io/tech-rnd-project/todo
 echo "process 4 running"
 wait $pid
 echo "process 4 done"
